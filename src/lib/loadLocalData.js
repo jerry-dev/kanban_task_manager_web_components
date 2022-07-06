cbef2a3a37dab09eb821a1e2c270e937699dee05
JerryD <jerry.webdev@gmail.com>
2022-07-06 08:30:38 -0400

    REFACTOR(loadLocalData.j): Add Note As Comment About SessionStorage

--- src/lib/loadLocalData.js
+++ src/lib/loadLocalData.js
@@ -1,8 +1,9 @@
 import mockData from '../data/mockdata.json' assert { type: 'json' };
 
+// Might need to save the store's state.js data within session storage
 const loadLocalData = (dispatch, isViewDataReady) => {
     if (!isViewDataReady) {
-        dispatch({type: 'LOAD_APPLICATION_DATA', payload: JSON.parse(mockData.boards)});
+        dispatch({type: 'LOAD_APPLICATION_DATA', payload: mockData.boards});
         return dispatch({type: 'IS_APPLICATION_DATA_READY', payload: true});
     } else {
         return dispatch({type: 'IS_APPLICATION_DATA_READY', payload: true});
