f60b13d6bd31a13293768ac1603793bac0b6df94
JerryD <jerry.webdev@gmail.com>
2022-07-06 09:30:45 -0400

    ADD(loadApplicationData.js): Add Action That Injects The Application Data Into The Data Store

---
+++ src/lib/store/actions/loadApplicationData.js
@@ -0,0 +1,5 @@
+//Injects the data into the state
+export default function(context, payload) {
+    console.info('Loading the application data into the data store');
+    context.commit('LOAD_APPLICATION_DATA', payload);
+}
