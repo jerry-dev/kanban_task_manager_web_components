1b72d3732ff646ae0d4dadd881a0844d37dedd08
JerryD <jerry.webdev@gmail.com>
2022-07-11 22:50:34 -0400

    UPDATE(actions/updateTask.js): Add Ability To Repsoition A Task To The Newly Selected Column Based On Status

--- src/lib/store/actions/updateTask.js
+++ src/lib/store/actions/updateTask.js
@@ -1,4 +1,12 @@
 export default function(context, payload) {
+    let statusChange = false;
+    const oldColumnPositionDetails = {
+        board: null,
+        oldColumnPosition: null,
+        oldTaskPosition: null,
+        newColumn: null
+    };
+
     let currentBoard = payload.boardName.split("");
     currentBoard[0] = currentBoard[0].toUpperCase();
 
@@ -16,17 +24,40 @@ export default function(context, payload) {
         if (state.boards[i].name === currentBoard) {
             for (let j = 0; j < state.boards[i].columns.length; j++) {
                 if (state.boards[i].columns[j].name === payload.columnName) {
-                    
                     for (let y = 0; y < state.boards[i].columns[j].tasks.length; y++) {
                         if (state.boards[i].columns[j].tasks[y].title === payload.taskTitle) {
-                            state.boards[i].columns[j].tasks[y].status = payload.newStatus
-                            state.boards[i].columns[j].tasks[y].subtasks = payload.subtasks
+                            //Checking if the status has changes
+                            if (state.boards[i].columns[j].tasks[y].status !== payload.newStatus) {
+                                // If the column
+                                statusChange = !statusChange;
+                                // Capturing the old position of the task to later cut and paste it
+                                // to the new position.
+                                oldColumnPositionDetails.board = i;
+                                oldColumnPositionDetails.oldColumnPosition = j;
+                                oldColumnPositionDetails.oldTaskPosition = y;
+                                oldColumnPositionDetails.newColumn = payload.newStatus;
+                            }
+                            state.boards[i].columns[j].tasks[y].status = payload.newStatus;
+                            state.boards[i].columns[j].tasks[y].subtasks = payload.subtasks;
                         }
                     }
                 }
             }
         }
     }
+
+    if (statusChange) {
+        const boardPos = oldColumnPositionDetails.board;
+        const colPos = oldColumnPositionDetails.oldColumnPosition;
+        const taskPos = oldColumnPositionDetails.oldTaskPosition;
+
+        for (let i = 0; i < state.boards[boardPos].columns.length; i++) {
+            if (state.boards[boardPos].columns[i].name === oldColumnPositionDetails.newColumn) {
+                state.boards[boardPos].columns[i].tasks.push(JSON.parse(JSON.stringify(state.boards[boardPos].columns[colPos].tasks[taskPos])));
+                state.boards[boardPos].columns[colPos].tasks.splice(taskPos, 1);
+            }
+        }
+    }    
 
     context.commit({ type: 'UPDATE_TASK', payload: state });
 }
