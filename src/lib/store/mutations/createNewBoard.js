6bb26c714835f2892f98bbf35d39c798ef6ba262
JerryD <jerry.webdev@gmail.com>
2022-07-20 09:43:18 -0400

    FIX(mutations/createNewBoard.js): Changed The Payload From Overwriting state.boards To Appended To state.boards

--- src/lib/store/mutations/createNewBoard.js
+++ src/lib/store/mutations/createNewBoard.js
@@ -1,6 +1,6 @@
 export default function(state, payload) {
     const { ...newState } = state;
-    newState.boards = payload;
+    newState.boards[newState.boards.length] = payload;
     sessionStorage.setItem('appState', JSON.stringify(newState));
     return newState;
 }
