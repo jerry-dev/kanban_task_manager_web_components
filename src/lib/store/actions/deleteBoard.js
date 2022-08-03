8dc7ebdf437f422054bd564f63861e2bc858d1db
JerryD <jerry.webdev@gmail.com>
2022-08-03 19:02:38 -0400

    FIX(actions/deleteBoard.js): Fix The Bug That's Causing The Application To Not Be ABle To Delete A Board
    Problem
    Was not able to delete a test board called "DMX"
    
    Solution
    - Removed the payload.board property called "name". payload.board features the name.
    - Used toLowerCase() On The Elements We're Trying To Match

--- src/lib/store/actions/deleteBoard.js
+++ src/lib/store/actions/deleteBoard.js
@@ -2,7 +2,7 @@ export default function(context, payload) {
     const boards = JSON.parse(JSON.stringify(context.state.boards))
 
     for (let i = 0; i < boards.length; i++) {
-        if (boards[i].name === payload.board) {
+        if (boards[i].name.toLowerCase() === payload.board.toLowerCase()) {
             boards.splice(i, 1);
         }
     }
