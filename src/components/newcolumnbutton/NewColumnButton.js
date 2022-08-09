26c38d95d224d80535201e1dd411f3b5cab0982f
JerryD <jerry.webdev@gmail.com>
2022-08-09 10:02:28 -0400

    MODULE CLEANUP(NewColumnButton.js): Removed Useless Commented Out Content

--- src/components/newcolumnbutton/NewColumnButton.js
+++ src/components/newcolumnbutton/NewColumnButton.js
@@ -227,7 +227,6 @@ export default class NewColumnButton extends HTMLElement {
         })
     }
 
-    // ---------------------------------------------------------------------------
     // Invoke if the originalBoardNameStatus === "CHANGED"
     // Skip this check if the originalBoardNameStatus === "SAME"
     doesBoardNameExist(newBoardName, store) {
@@ -288,7 +287,6 @@ export default class NewColumnButton extends HTMLElement {
         messageContainer.classList.add('error');
         messageContainer.innerText = `At least one column name you specified is too similar to an existing column name in this board. Please specify another name.`;
     }
-    // END------------------------------------------------------------------------
 
     submitEditBoardDialogFormManager() {
         let isWatchingBoardNameInput = false;
