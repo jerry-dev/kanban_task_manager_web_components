13541fbbc1b3cab2981e76c7fee3a86e876f71c8
JerryD <jerry.webdev@gmail.com>
2022-08-09 15:01:40 -0400

    UPDATE(rollup.config.js): Configure The @rollup/plugin-replace Plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { importAssertionsPlugin } from 'rollup-plugin-import-assert';
 import { importAssertions } from 'acorn-import-assertions';
 import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
 import modify from 'rollup-plugin-modify';
 
 const devMode = (process.env.NODE_ENV === 'development');
@@ -14,7 +15,13 @@ export default [
 			include: './src/**',
 			clearScreen:  false
 		},
-		
+		plugins: [
+			replace({
+				values: {
+					"../src/assets": "./dist/assets"
+				}
+			})
+		],
 		output: {
 			file: './dist/bundle.js',
 			format: 'es',
